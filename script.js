617374fe5d722f7edf83957d29e41218bf5bf3cb
Angus <angusclark93@gmail.com>
2018-03-08 22:49:54 +0600

    Finish styling start JS
    - Style Tasks + Completed sections
    - write function to clear field
    - write function to create new element (incomplete)

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
+(function() {
 
+// Clear field when 'Clear' button clicked
 
-//User clicked Submit
-
-//If there is text inside entry field, add text to toDo list
-
-document.getElementById('submit').addEventListener('click', function() {
-
-let value = document.getElementById('entry').value;
-
-if (value) { addItemTodo(value)
-
-};
-
+const clear = document.getElementById('clear_button');
+clear.addEventListener('click', function() {
+    const input = document.getElementById('input');
+    input.value = '';
 });
 
+// Print task in 'Tasks' when 'Submit' button clicked
 
-function addItemTodo() {
+const submit = document.getElementById('submit_button');
 
-};
+submit.addEventListener('click', function() {
+
+
+    let newItem = document.createElement('li');  // create new li
+    let tasks = document.getElementById('tasks');
+
+    tasks.appendChild(newItem);
+    newItem.innerText = input.value;  // print user input in new li
+
+    if (input.value !== '') {
+        input.value = '';
+      }
+});
+})();
