7d5a9870fe8cb61746a50ae5ea7102681956a020
Angus <angusclark93@gmail.com>
2018-03-11 22:37:04 +0600

    Implement functionality for 1 task
    - DOM Manipulation for 1 task
    - Problem: work out how to repeat functionality

--- script.js
+++ script.js
@@ -1,28 +1,91 @@
 (function() {
+  var domElements = [
 
-// Clear field when 'Clear' button clicked
+     tasks = document.getElementById('tasks'),
+     clear = document.getElementById('clear_button'),
+     submit = document.getElementById('submit_button'),
+     checkButton = document.getElementsByClassName('check'),
+     deleteButton = document.getElementsByClassName('delete'),
+     example = document.getElementById('example'),
+     secondaryButtons = document.getElementById('secondary_buttons')
+  ];
 
-const clear = document.getElementById('clear_button');
-clear.addEventListener('click', function() {
-    const input = document.getElementById('input');
+  // Clear field when 'Clear' button clicked
+  clear.addEventListener('click', function() {
+    var input = document.getElementById('input');
     input.value = '';
-});
+  });
 
-// Print task in 'Tasks' when 'Submit' button clicked
+  // append user input to new li
+  function newItem() {
+    var newLi = document.createElement('li');
+    newLi = tasks.insertBefore(newLi, secondaryButtons); // append new li to ul '#tasks'
+    newLi.innerText = input.value; // print user input in new li
+    //var newSecondaryButtons = document.createElement('li');
+    //newSecondaryButtons.appendChild(newSecondaryButtons);
+  };
 
-const submit = document.getElementById('submit_button');
+// 'Submit' Clicked
+  submit.addEventListener('click', function() {
 
-submit.addEventListener('click', function() {
+    if (input.value === "") {
+      alert('You must first enter a task');
+    } else {
+      newItem();
+      deleteExample();
+      input.value = "";
+    }
+  });
+
+  function deleteExample() {
+    if (example !== "") {
+    example.parentNode.removeChild(example);
+  } else {
+
+  }
+
+    //checkButton[0].parentNode.removeChild(checkButton[0]);
+    //deleteButton[0].parentNode.removeChild(deleteButton[0]);
+  }
 
 
-    let newItem = document.createElement('li');  // create new li
-    let tasks = document.getElementById('tasks');
+// Object containing check & delete buttons (arrays)
 
-    tasks.appendChild(newItem);
-    newItem.innerText = input.value;  // print user input in new li
+  var secondaryButtonsObj = {
 
-    if (input.value !== '') {
-        input.value = '';
-      }
-});
-})();
+    checkButtons: [
+      checkButton[0]
+    ],
+
+    deleteButtons: [
+      deleteButton[0]
+    ]
+  };
+
+    secondaryButtonsObj.checkButtons[0].addEventListener('click', function() {
+
+    var completed = document.getElementById('completed');
+    var newLi = document.createElement('li');
+    completed.appendChild(newLi);
+    newLi.innerHTML = tasks.firstElementChild.innerHTML;
+    this.parentNode.removeChild(this);
+    deleteButton[0].parentNode.removeChild(deleteButton[0]);
+
+  });
+console.dir(tasks);
+  secondaryButtonsObj.deleteButtons[0].addEventListener('click', function(){
+
+
+  })
+
+
+  /*
+          var example = tasks.childNodes[1];
+        ;
+          ;
+          alert('clicked');
+          //completed.appendChild(newLi);
+          //newLi.innerText = "New list item";
+ */
+
+}());
